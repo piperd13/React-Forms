effa12cb224bc0fb0cf20f6a24c65ae5038267f6
Piper Downs <piperdowns@Pipers-MBP.localdomain>
2023-08-10 10:04:17 -0500

    completed assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import SignUpForm from './components/SignUpForm.jsx';
+import Authenticate from './components/Authenticate.jsx';
 
-function App() {
-
+export default function App() {
+  const [token, setToken] = useState(null);
   return (
     <>
-
+    <SignUpForm token={token} setToken={setToken}/>
+    <Authenticate token={token} setToken={setToken}/>
     </>
-  )
+  );
 }
-
-export default App
---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {useState, useEffect} from 'react';
+
+export default function SignUpForm({setToken}) {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+
+        try {
+            const response = await fetch(`https://fsa-jwt-practice.herokuapp.com/signup`, {
+                method: "POST",
+                body: JSON.stringify({username}, {password})
+            });
+            const result = await response.json();
+            setToken(result.token);
+            console.log(result);
+        } catch (error) {
+            setError(error.message);
+        }
+    };
+        return (
+        <>
+        <h2>Sign Up!</h2>
+        {error && <p>{error}</p>}
+        <form onSubmit={handleSubmit}>
+            <label>
+                Username: {" "} <input value={username} onChange={(e) => setUsername(e.target.value)}/>
+            </label>
+            <label>
+                Password: {" "} <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+            </label>
+            <button>Submit</button>
+        </form>
+        </>
+        )
+}
